49304306d618f93a0dc1127175e719230226d398
junhui.teo <e0203169@u.nus.edu>
2020-02-15 15:44:07 -0800

    check audio

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 from flask import render_template, request
 from flask import Flask
+from read_audio import text_to_speech
 
 app = Flask(__name__)
 
@@ -12,6 +13,12 @@ def index():
 def get():
     user_name = request.form.get('userName')
     return render_template('base.html', username=user_name)
+
+@app.route('/response', methods=['POST'])
+def process_response():
+    data = request.get_json()
+    text = data["text"]
+    text_to_speech(text)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- read_audio.py
+++ read_audio.py
@@ -1,15 +1,23 @@
 import speech_recognition as sr
-from gtts import gTTS
 import os
 import pygame
 import houndify
+import base64
 
 # use the audio file as the audio source
 r = sr.Recognizer()
 
 HOUNDIFY_CLIENT_ID = "94iXzZrdKCISkoWU-y1teQ=="  # Houndify client IDs are Base64-encoded strings
 HOUNDIFY_CLIENT_KEY = "_7OwfiGjU6q8ppWY5NuP7AikDNt9JQo02ke3nNv39leZNz8DPwxZyOOO9Oh6ErMrvRKv8NFk_aOPiUhfQ4kxIA=="
-client = houndify.TextHoundClient(HOUNDIFY_CLIENT_ID, HOUNDIFY_CLIENT_KEY, "test")
+userId = "test_user"
+requestInfo = {
+  "Latitude": 37.388309,
+  "Longitude": -121.973968,
+  "ResponseAudioVoice": "Judy",
+  "ResponseAudioShortOrLong": "Short"
+}
+
+client = houndify.TextHoundClient(HOUNDIFY_CLIENT_ID, HOUNDIFY_CLIENT_KEY, userId, requestInfo)
 # Houndify client keys are Base64-encoded strings
 
 
@@ -25,22 +33,18 @@ def record_query():
 # Print the Translated text
     print(text)
 
-def text_to_speech(text, language='en'):
-
-    # Passing the text and language to the engine,
-    # here we have marked slow=False. Which tells
-    # the module that the converted audio should
-    # have a high speed
-    myobj = gTTS(text=text, lang=language, slow=False)
-
-    # Saving the converted audio in a mp3 file named
-    # welcome
-    try:
-        myobj.save("")
-    except:
-        print("Error writing string to mp3")
-
-    # Playing the converted file
+def playSound(filename):
     pygame.mixer.init()
-    os.system("mpg321 welcome.mp3")
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+
+def text_to_speech(text):
+    response = client.query(text).json()
+    print(response)
+    response = base64.b64decode(response["ResponseAudioBytes"])
+    with open('myfile.wav', mode='bx') as f:
+        f.write(response)
+    playSound('myfile.wav')
+    os.remove('myfile.wav')
+
 
