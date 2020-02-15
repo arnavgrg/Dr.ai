e379e7d530761ec366ad345730e545dcf80808e7
junhui.teo <e0203169@u.nus.edu>
2020-02-15 05:29:51 -0800

    Houndify template works

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Flask app is working"
+
+
+@app.route('/query')
+def get():
+    return render_template('base.html')
