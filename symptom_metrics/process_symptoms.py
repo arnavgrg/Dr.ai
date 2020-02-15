5be12bfe40651d5de6e4dd1d8ce93b23978b07be
Siddharth Verma <siddharthverma314@gmail.com>
2020-02-15 15:45:32 -0800

    Initial process test

--- symptom_metrics/process_symptoms.py
+++ symptom_metrics/process_symptoms.py
@@ -1,18 +1,23 @@
 ''' Functions for symptom matching between user and database '''
 import json
-import bert
+from bert_serving.client import BertClient
+import pickle
 
 SYMPTOMS_FILE = "../symptoms.json"
 
 
 def parse_symptoms():
     ''' Parse symptoms and run it through bert '''
+    bc = BertClient(check_length=False)
+
     with open(SYMPTOMS_FILE) as f:
         symptoms = json.load(f)
 
-    for symptom in symptoms.keys():
-        bert
+    symptoms = symptoms.keys()
+    encodings = bc.encode(symptoms)
 
+    print(encodings[0])
+    print(len(encodings))
 
 def matched_symptoms(user_text):
     known_symptoms = load_symptoms()
@@ -20,4 +25,6 @@ def matched_symptoms(user_text):
     return query_words.intersection(known_symptoms)
 
 
-print(matched_symptoms('I might be catching the flu'))
+#print(matched_symptoms('I might be catching the flu'))
+if __name__ == "__main__":
+    parse_symptoms()
